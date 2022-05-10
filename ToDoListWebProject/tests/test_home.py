5db5bf5f154e3c69e24f021bbba99fb668bd7e7a
gongna <2036479155@qq.com>
2022-05-11 01:09:13 +0800

    add two project

---
+++ ToDoListWebProject/tests/test_home.py
@@ -0,0 +1,55 @@
+
+import unittest
+
+from flask import url_for
+
+from todoism import create_app, db
+
+
+class HomeTestCase(unittest.TestCase):
+
+    def setUp(self):
+        app = create_app('testing')
+        self.app_context = app.test_request_context()
+        self.app_context.push()
+        db.create_all()
+        self.client = app.test_client()
+
+    def tearDown(self):
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_index(self):
+        response = self.client.get(url_for('home.index'))
+        data = response.get_data(as_text=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Todoism', data)
+
+    def test_intro(self):
+        response = self.client.get(url_for('home.intro'))
+        data = response.get_data(as_text=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('This Is A TodoList Online WebSite', data)
+
+    def test_change_language(self):
+        response = self.client.get(url_for('home.intro'))
+        data = response.get_data(as_text=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('TodoList Online WebSite', data)
+        self.assertIn('Login', data)
+
+        response = self.client.get(url_for('home.set_locale', locale='JP'))
+        data = response.get_json()
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['message'], 'Invalid locale.')
+
+        response = self.client.get(url_for('home.set_locale', locale='zh_Hans_CN'))
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['message'], 'Setting updated.')
+
+        response = self.client.get(url_for('home.intro'))
+        data = response.get_data(as_text=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(u'TodoList Online WebSite', data)
+        self.assertIn(u'登录', data)
